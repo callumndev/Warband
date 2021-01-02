40481fc2fba406d5d53e022bbb128789768361a0
lz0408 <callumnewsome100@gmail.com>
2021-01-02 23:02:29 +0000

    remove some ready things

--- src/events/readyMessage.js
+++ src/events/readyMessage.js
@@ -1,14 +1,4 @@
-module.exports.run = () => {
-    const readyMsg = '[Warband] ' + bot.user.tag + ' is online';
-
-    console.log( readyMsg );
-
-    let nameRn = discord.channels.lockedLounge.name;
-
-    discord.channels.lockedLounge.setName( readyMsg );
-
-    setTimeout( () => discord.channels.lockedLounge.setName( nameRn ), 3000 );
-};
+module.exports.run = () => console.log( '[Warband] ' + bot.user.tag + ' is online' );
 
 module.exports.config = {
     event: 'ready',
